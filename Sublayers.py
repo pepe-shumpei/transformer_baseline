e010d297e508a24b5fce7c4948cf61b238fcba6a
pepe-shumpei <shun.515515@icloud.com>
2020-09-18 16:13:16 +0900

    add dropout in ff

--- Sublayers.py
+++ Sublayers.py
@@ -65,11 +65,10 @@ class FeedForward(nn.Module):
     
         # We set d_ff as a default to 2048
         self.linear_1 = nn.Linear(d_model, d_ff)
-        #self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
         self.linear_2 = nn.Linear(d_ff, d_model)
     
     def forward(self, x):
-        #x = self.dropout(F.relu(self.linear_1(x)))
-        x = F.relu(self.linear_1(x))
+        x = self.dropout(F.relu(self.linear_1(x)))
         x = self.linear_2(x)
         return x
